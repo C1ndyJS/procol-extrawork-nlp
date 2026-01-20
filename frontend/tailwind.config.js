1608402a2c71e55eb92482f38ead04f36b55634f
C1ndyjs <cindy.jimenez@utp.edu.co>
2026-01-20 18:17:50 -0500

    Estilos Them Azure

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: '#e8f4f8',
+          100: '#a2d5e7',
+          200: '#1e90ff',
+          300: '#2e6fa8',
+          400: '#1a4d7a',
+        },
+      },
+    },
   },
   plugins: [],
 };
